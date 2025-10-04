b2e8f31bad8b2f63d93e6f48860d7dd481aa6136
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-04 10:12:39 +0530

    Functiional components

--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //   </React.StrictMode>
 // );
 
-console.log("Hello World");
+function App() {
+  return <ul><li>Priyanshu</li> <li>Krishna</li></ul>
+}
 // the HTML inside the render thing is called jsx and we can only pass single element to the render function.
-root.render(<div><h1>Priyanshu SINGH</h1> <p>Krishna</p></div>)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

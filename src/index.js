c15561b0fa1c30ed36c23853e435166ee3bf98f4
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-04 10:02:00 +0530

    Update index.html and index.js to modify content and structure. Added a greeting in index.js and adjusted HTML formatting in index.html.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+console.log("Hello World");
+// the HTML inside the render thing is called jsx and we can only pass single element to the render function.
+root.render(<div><h1>Priyanshu SINGH</h1> <p>Krishna</p></div>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

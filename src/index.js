0d87ec729fd4b01ad94349cc98a75d5b11801c73
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-04 10:37:53 +0530

    Refactor index.js to replace App component with Myapp component, encapsulating the previous content in a new file Myapp.js.

--- src/index.js
+++ src/index.js
@@ -2,23 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
+import Myapp from './Myapp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function App() {
-  return <div className="container"><h1>Priyanshu SINGH</h1>
-    <p>I am priyanshu Singh from uttar pradesh gorakhpur ,compeleted my Btech in computer science and engineering from Nit Hamirpur, Gorakhpur</p>
-    <ul>
-      <li>Goa </li>
-      <li>Himachal Pradesh</li>
-      <li>Japan</li>
-      <li>China</li>
-    </ul>
-  </div >
-
-}
 // the HTML inside the render thing is called jsx and we can only pass single element to the render function.
-root.render(<App />)
+root.render(<Myapp />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

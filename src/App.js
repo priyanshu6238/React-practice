becf5043acc0211e848de49a102d74bdad31a750
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-09 16:11:41 +0530

    Add jokes data to joker.js for use in the application

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Myapp from "./components/Myapp";
 import Footer from "./components/Footer";
 
 function App() {
+  const nums = [1, 2, 3, 4, 5];
+  const new_num = nums.map((n) => n * 2);
+  console.log(new_num);
   return (
     <div>
       <Myapp />

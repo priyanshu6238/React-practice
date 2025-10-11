2fc86e42070fa8a22d678b0515790c01c542631c
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-11 18:38:12 +0530

    Enhance class_component to include additional state variables and conditional rendering for dynamic display text

--- src/class_component.js
+++ src/class_component.js
@@ -6,15 +6,34 @@ class class_component extends React.Component {
     constructor() {
         super()
         this.state = {
-            answer: "Priyanshu Singh"
+            answer: "Priyanshu Singh",
+            bol: true,
+            answer2: "Deepanshu Singh"
         }
+
     }
+
+
     render() {
+
+        let display_text;
+
+        if (this.state.bol) {
+            display_text = this.state.answer;
+        }
+        else {
+            display_text = this.state.answer2
+        }
 
         return (
             < div >
 
-                <h1> {this.state.answer} </h1>
+
+                <h1> {display_text} </h1>
+
+
+
+
 
             </div >
         )

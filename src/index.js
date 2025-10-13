39a4348ec4d50b3a4b827c6e406a2191df72df74
Priyanshu singh <111607560+priyanshu6238@users.noreply.github.com>
2025-10-13 14:01:12 +0530

    On click project in class (#6)
    * Add Project_on_click component with click counter functionality and associated styles
    
    * Add Project_loading component to handle loading state and update index.js to render it instead of Project_on_click

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./App.css";
 import reportWebVitals from "./reportWebVitals";
 import Project_on_click from "./Project_On_click";
+import Project_on_click from "./Project_On_click";
 // import Myapp from "./components/Myapp";
+import Project_loading from "./Project_loading"
 import App from "./App";
 import Class_component from "./class_component";
 
@@ -11,7 +13,7 @@ import Class_component from "./class_component";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // the HTML inside the render thing is called jsx and we can only pass single element to the render function.
-root.render(<Project_on_click />);
+root.render(<Project_loading />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

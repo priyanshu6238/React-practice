fed31b93996467edb02e352d6308cc8c051ebfea
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-16 08:55:37 +0530

    api fetch in js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
-import reportWebVitals from "./reportWebVitals";
-import Project_on_click from "./Project_On_click";
-import Project_on_click from "./Project_On_click";
-// import Myapp from "./components/Myapp";
-import Project_loading from "./Project_loading"
-import App from "./App";
-import Class_component from "./class_component";
-
+// import "./App.css";
+// import reportWebVitals from "./reportWebVitals";
+// import Project_on_click from "./Project_On_click";
+// import Project_on_click from "./Project_On_click";
+// // import Myapp from "./components/Myapp";
+// import Project_loading from "./Project_loading";
+// import App from "./App";
+// import Class_component from "./class_component";
+import Api_fetch from "./Api_fetch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // the HTML inside the render thing is called jsx and we can only pass single element to the render function.
-root.render(<Project_loading />);
+root.render(<Api_fetch />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
